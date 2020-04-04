477e90db5096a060896b60cf15f049fcad2933cd
Lan Nguyen <lhngocnguyen@gmail.com>
2020-04-04 13:52:53 -0700

    created friend class and toggle ighlights

--- client/scripts/friends.js
+++ client/scripts/friends.js
@@ -1,4 +1,48 @@
 var Friends = {
+  usernames : {},
 
+  toggleStatus: function(newFriend) {
+    if (newFriend in Friends.usernames) {
+      delete newFriend;
+    } else {
+      Friends.usernames[newFriend] = true;
+    }
+  },
 
+  initialize: function() {
+    $('.username').on('click', event => {
+
+      var newFriend = $(event.currentTarget).html();
+      Friends.toggleStatus(newFriend);
+      //Friends.usernames[newFriend] = true;
+
+    });
+    Friends.highlight();
+  },
+
+  highlight: function () {
+    console.log(Friends.username);
+    for (friend in Friends.usernames) {
+      var chats = document.getElementsByClassName('chat');
+      //console.log('chats array', chats);
+      //chat.getIndex(friend)
+      for (chat of chats) {
+        //console.log('chat', chats);
+        if ($(chat).find('.username').html() === friend) {
+          //console.log('Username match');
+          $(chat).css('font-weight', 'bold');
+
+        } else {
+          $(chat).css('font-weight', 'normal');
+        }
+      }
+    }
+  }
 };
+
+
+    //   <div class="chat">
+    //     <div class="username"> %= username =% </div>
+    //     <div> <b> <%=text%>> </b> </div>
+    //   </div>
+    // `)
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -6,6 +6,7 @@ var MessagesView = {
     $('#chats').html('');
     Parse.readAll((data) => {
     MessagesView.render(data);
+    Friends.initialize();
     });
 
     window.setInterval( MessagesView.initialize, 10000);
@@ -18,7 +19,30 @@ var MessagesView = {
   },
 
   renderMessage: function(message) {
+    message.username = MessagesView.sanitize(message.username || "");
+    message.roomname = MessagesView.sanitize(message.roomname || "");
+    message.text = MessagesView.sanitize(message.text || "");
+
     var htmlString = MessageView.render(message);
     $('#chats').append(htmlString);
+  },
+
+  // functionhtmlEncode(str){
+  //   return String(str).replace(/[^\w. ]/gi, function(c){
+  //      return '&#'+c.charCodeAt(0)+';';
+  //   });
+  // }
+
+  sanitize: function(string) {
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#x27;',
+        "/": '&#x2F;',
+    };
+    const reg = /[&<>"'/]/ig;
+    return string.replace(reg, (match)=>(map[match]));
   }
 };
